e42c1f62e96ddc5edba1ce72069132175202f762
Jungar <asger@tang.dk>
2020-02-24 00:22:16 +0100

    removed several files

--- R/test.r
+++
@@ -1,8 +0,0 @@
-
-file = 'Data/wells.txt'
-wellData <- read.table(file, header = TRUE, sep = "", dec = ".")
-
-plot(data)
-
-summary <- glm(conc ~ nwell,family = poisson(link = log), data = wellData)
-plot(summary)
--- Untitled.R
+++
@@ -1,7 +0,0 @@
-file = 'Data/wells.txt'
-well.data <- read.table(file, header = TRUE, sep = "", dec = ".")
-
-summary <- glm(conc ~ nwell,family = poisson(link = log), data = well.data)
-
-
-plot(summary)
