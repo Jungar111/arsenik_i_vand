5195747d8fa66e5b6a86834830b1515d99d72f1c
Frederik Møller Sørensen <Frederik.moller2@hotmail.com>
2020-02-24 19:24:39 +0100

    Skrevet kommentarer, for bedre forståelse

--- R/Analyse.r
+++ R/Analyse.r
@@ -35,28 +35,30 @@ drop1(analysis, test="Chisq")
 prediction.temp<-as.data.frame(predict(analysis,se.fit=T))
 prediction.data<-data.frame(pred=prediction.temp$fit, upper=prediction.temp$fit+ 1.96*prediction.temp$se.fit, lower=prediction.temp$fit-1.96*prediction.temp$se.fit)
 
+# Transformerer dataen tilbage til original tilstand og sorterer data efter pred
 prediction.data.original <- exp(prediction.data)
 prediction.data.original <- prediction.data.original[order(prediction.data.original$pred),]
 
 
-# plots i original data transformation 
+# Sorterede plots i original data transformation 
 plot(prediction.data.original$pred, fblad$events[order(prediction.data.original$pred)], col="blue")
 lines(prediction.data.original$lower, fblad$events[order(prediction.data.original$pred)], col="red")
 lines(prediction.data.original$upper, fblad$events[order(prediction.data.original$pred)], col="red")
 
 
-#plot i log data 
+#plot i log transformation 
 plot(prediction.data$pred, fblad$events[order(prediction.data$pred)], col="blue")
 lines(prediction.data$lower, fblad$events, col="red")
 lines(prediction.data$upper, fblad$events, col="red")
 
-
+# Histogram over log transformeret procent for at få kræft
 hist(log(fblad$events/fblad$at.risk))
+
+# Data til histogrammet oven over
 cbind(fblad[fblad$events>4,] ,log(fblad$events/fblad$at.risk)[fblad$events > 4])
 
-
+# Taylor udvider til ny analysis
 analysis2 <- update(analysis, ~.+I(conc^2)+I(age^2))
-#analysis2 <- update(analysis2, ~.+I(conc^3))
 summary(analysis2)
 
 # Hvor god er modellen 
