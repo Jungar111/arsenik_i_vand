6f635f5363674c4df00e521657a32ff413a47efc
Jungar <asger@tang.dk>
2020-02-24 14:13:04 +0100

    Leg med Taylor

--- R/Analyse.r
+++ R/Analyse.r
@@ -1,8 +1,8 @@
 # Frederik wd
-setwd("C:\\Users\\frede\\OneDrive\\Dokumenter\\DTU\\4. Semester\\Fagprojekt\\ArsenikGit\\Data")
+# setwd("C:\\Users\\frede\\OneDrive\\Dokumenter\\DTU\\4. Semester\\Fagprojekt\\ArsenikGit\\Data")
 
 # Asger wd
-#setwd("/Users/AsgerSturisTang/OneDrive - Danmarks Tekniske Universitet/DTU/4. Semester/Arsenik i vand/Data")
+setwd("/Users/AsgerSturisTang/OneDrive - Danmarks Tekniske Universitet/DTU/4. Semester/Arsenik i vand/Data")
 
 # Joachim wd
 #setwd("/Users/JoachimPorsA/Documents/4. Semester - DTU/Fagprojekt/Data/Arsenik i vand/Data")
@@ -25,6 +25,8 @@ p.hat <- sum(fblad$events/N)
 analysis<-glm(events~conc+age,family=poisson(link=log),data=fblad,offset=log(at.risk)) 
 summary(analysis)
 
+
+
 # Hvor god er modellen 
 drop1(analysis, test="Chisq")
 
@@ -32,14 +34,14 @@ drop1(analysis, test="Chisq")
 prediction.temp<-as.data.frame(predict(analysis,se.fit=T))
 prediction.data<-data.frame(pred=prediction.temp$fit, upper=prediction.temp$fit+ 1.96*prediction.temp$se.fit, lower=prediction.temp$fit-1.96*prediction.temp$se.fit)
 
-prediction.data.original<-exp(prediction.data)
+prediction.data.original <- exp(prediction.data)
 prediction.data.original <- prediction.data.original[order(prediction.data.original$pred),]
 
 
 # plots i original data transformation 
 plot(prediction.data.original$pred, fblad$events[order(prediction.data.original$pred)], col="blue")
-lines(prediction.data.original$lower, fblad$events, col="red")
-lines(prediction.data.original$upper, fblad$events, col="red")
+lines(prediction.data.original$lower, fblad$events[order(prediction.data.original$pred)], col="red")
+lines(prediction.data.original$upper, fblad$events[order(prediction.data.original$pred)], col="red")
 
 
 #plot i log data 
@@ -51,3 +53,28 @@ lines(prediction.data$upper, fblad$events, col="red")
 hist(log(fblad$events/fblad$at.risk))
 cbind(fblad[fblad$events>4,] ,log(fblad$events/fblad$at.risk)[fblad$events > 4])
 
+
+analysis2 <- update(analysis, ~.+I(conc^2) + I(age^2))
+analysis2 <- update(analysis2, ~.+I(conc^3) + I(age^3))
+summary(analysis2)
+
+# Hvor god er modellen 
+drop1(analysis2, test="Chisq")
+
+# Laver signifikans niveauer 
+prediction.temp<-as.data.frame(predict(analysis2,se.fit=T))
+prediction.data<-data.frame(pred=prediction.temp$fit, upper=prediction.temp$fit+ 1.96*prediction.temp$se.fit, lower=prediction.temp$fit-1.96*prediction.temp$se.fit)
+
+prediction.data.original <- exp(prediction.data)
+prediction.data.original <- prediction.data.original[order(prediction.data.original$pred),]
+
+# plots i original data transformation 
+plot(prediction.data.original$pred[prediction.data.original<4], fblad$events[prediction.data.original<4], col="blue")
+lines(prediction.data.original$lower, fblad$events[order(prediction.data.original$pred)], col="red")
+lines(prediction.data.original$upper, fblad$events[order(prediction.data.original$pred)], col="red")
+
+
+#plot i log data 
+plot(prediction.data$pred, fblad$events[order(prediction.data$pred)], col="blue")
+lines(prediction.data$lower, fblad$events, col="red")
+lines(prediction.data$upper, fblad$events, col="red")
