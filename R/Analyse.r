74cba901628168cf049f3051d8d779cebd9f0f25
Jungar <asger@tang.dk>
2020-03-02 15:20:04 +0100

    Småændringer

--- R/Analyse.r
+++ R/Analyse.r
@@ -1,30 +1,35 @@
 # Frederik wd
-#setwd("C:\\Users\\frede\\OneDrive\\Dokumenter\\DTU\\4. Semester\\Fagprojekt\\ArsenikGit\\Data")
+# setwd("C:\\Users\\frede\\OneDrive\\Dokumenter\\DTU\\4. Semester\\Fagprojekt\\ArsenikGit\\Data")
 
 # Asger wd
-#setwd("/Users/AsgerSturisTang/OneDrive - Danmarks Tekniske Universitet/DTU/4. Semester/Arsenik i vand/Data")
+setwd("/Users/AsgerSturisTang/OneDrive - Danmarks Tekniske Universitet/DTU/4. Semester/Arsenik i GIT/Data")
 
 # Joachim wd
 #setwd("/Users/JoachimPorsA/Documents/4. Semester - DTU/Fagprojekt/Data/Arsenik i vand/Data")
 
 #Oskar wd 
- setwd("C:\\Users\\User\\OneDrive - Danmarks Tekniske Universitet\\SAS_030919\\4. Semester\\42584_Fagprojekt\\Arsenik i drikkevand\\42584_Data\\arsenik_i_vand\\Data")
+# setwd("C:\\Users\\User\\OneDrive - Danmarks Tekniske Universitet\\SAS_030919\\4. Semester\\42584_Fagprojekt\\Arsenik i drikkevand\\42584_Data\\arsenik_i_vand\\Data")
 
 set.seed(69)
 
 fblad <- read.table("fblad.sw.dat", header=TRUE)
-head(fblad)
-#fblad <- fblad[c(15:549), ]
+mblad <- read.table("mblad.sw.dat", header=TRUE)
+fblad$gender <- "Female"
+mblad$gender <- "Male"
+blad <- rbind(fblad,mblad)
+
+head(blad)
+#blad <- blad[c(15:549), ]
 
 # Antal observationer
-N <- length(fblad$events)
+N <- length(blad$events)
 
 # p.hat (empirisk sandsynlighed)
-p.hat <- sum(fblad$events/N)
+p.hat <- sum(blad$events/N)
 
 
 # Her defineres modellen:
-analysis<-glm(events~sqrt(conc)+age,family=poisson(link=log),data=fblad,offset=log(at.risk)) 
+analysis<-glm(events~conc+age,family=poisson(link=log),data=blad,offset=log(at.risk)) 
 summary(analysis)
 
 
@@ -42,29 +47,30 @@ prediction.data.original <- prediction.data.original[order(prediction.data.origi
 
 
 # Sorterede plots i original data transformation 
-plot(prediction.data.original$pred, fblad$events[order(prediction.data.original$pred)], col="blue")
-lines(prediction.data.original$lower, fblad$events[order(prediction.data.original$pred)], col="red")
-lines(prediction.data.original$upper, fblad$events[order(prediction.data.original$pred)], col="red")
+plot(prediction.data.original$pred, blad$events[order(prediction.data.original$pred)], col="blue")
+lines(prediction.data.original$lower, blad$events[order(prediction.data.original$pred)], col="red")
+lines(prediction.data.original$upper, blad$events[order(prediction.data.original$pred)], col="red")
 
 
 # Laver foreløbig test, tror det her er den rigtige måde at plotte det på
-cbind(fblad$events[order(fblad$events, decreasing = TRUE)],round(prediction.data.original$pred,1))
-plot(fblad$events[order(fblad$events, decreasing = TRUE)],round(prediction.data.original$pred,0))
+cbind(blad$events[order(blad$events, decreasing = TRUE)],round(prediction.data.original$pred,1))
+plot(blad$events[order(blad$events, decreasing = TRUE)],round(prediction.data.original$pred,0))
 
 
 #plot i log transformation 
-plot(prediction.data$pred, fblad$events[order(prediction.data$pred)], col="blue")
-lines(prediction.data$lower, fblad$events, col="red")
-lines(prediction.data$upper, fblad$events, col="red")
+plot(prediction.data$pred, blad$events[order(prediction.data$pred)], col="blue")
+lines(prediction.data$lower, blad$events, col="red")
+lines(prediction.data$upper, blad$events, col="red")
 
 # Histogram over log transformeret procent for at få kræft
-hist(log(fblad$events/fblad$at.risk))
+hist(log(blad$events/blad$at.risk))
 
 # Data til histogrammet oven over
-cbind(fblad[fblad$events>4,] ,log(fblad$events/fblad$at.risk)[fblad$events > 4])
+cbind(blad[blad$events>4,] ,log(blad$events/blad$at.risk)[blad$events > 4])
 
 # Taylor udvider til ny analysis
-analysis2 <- update(analysis, ~.+I(sqrt(conc)^2)+I(age^2))
+analysis2 <- update(analysis, ~.+I(1/2*conc^2)+I(1/2*age^2))
+analysis2 <- update(analysis2, ~.+I(1/3*conc^3))
 summary(analysis2)
 
 # Hvor god er modellen 
@@ -75,15 +81,14 @@ prediction.temp<-as.data.frame(predict(analysis2,se.fit=T))
 prediction.data<-data.frame(pred=prediction.temp$fit, upper=prediction.temp$fit+ 1.96*prediction.temp$se.fit, lower=prediction.temp$fit-1.96*prediction.temp$se.fit)
 
 prediction.data.original <- exp(prediction.data)
-prediction.data.original <- prediction.data.original[order(fblad$events, decreasing = TRUE),]
+prediction.data.original <- prediction.data.original[order(blad$events, decreasing = TRUE),]
 
 
 # plots i original data transformation 
-plot(prediction.data.original$pred, fblad$events, col="blue")
-lines(prediction.data.original$lower, fblad$events, col="red")
-lines(prediction.data.original$upper, fblad$events, col="red")
+#plot(prediction.data.original$pred, blad$events, col="blue")
+#lines(prediction.data.original$lower, blad$events, col="red")
+#lines(prediction.data.original$upper, blad$events, col="red")
 
-<<<<<<< HEAD
 maxr <- 5
 
 # Laver foreløbig test, tror det her er den rigtige måde at plotte det på
@@ -104,17 +109,9 @@ for (i in 1:length(prediction.data.original$pred)){
 plot(x, v, xlim=c(0, maxr), ylim=c(0, maxr))
 lines(0:maxr,0:maxr, type="l")
 
-=======
-
-# Laver foreløbig test, tror det her er den rigtige måde at plotte det på
-cbind(fblad$events[order(fblad$events, decreasing = TRUE)],round(prediction.data.original$pred,1))
-plot(fblad$events[order(fblad$events, decreasing = TRUE)],round(prediction.data.original$pred,2), xlim=c(0, 25), ylim=c(0, 25))
-fblad$events[order(fblad$events, decreasing = TRUE)]
->>>>>>> 48ecb42d727d5ea803ac9d948bcc57e929b63ac5
 
 
 #plot i log data 
-plot(prediction.data$pred, fblad$events[order(prediction.data$pred)], col="blue")
-lines(prediction.data$lower, fblad$events, col="red")
-lines(prediction.data$upper, fblad$events, col="red")
-
+plot(prediction.data$pred, blad$events[order(prediction.data$pred)], col="blue")
+lines(prediction.data$lower, blad$events, col="red")
+lines(prediction.data$upper, blad$events, col="red")
