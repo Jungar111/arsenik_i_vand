859f4f33bcf69b857e4caeb32b8dcb31fb94370d
TheCocoMaan <joachimpors@hotmail.com>
2020-03-09 11:39:03 +0100

    Enkel kode

--- R/Analyse.r
+++ R/Analyse.r
@@ -1,14 +1,5 @@
-# Frederik wd
-# setwd("C:\\Users\\frede\\OneDrive\\Dokumenter\\DTU\\4. Semester\\Fagprojekt\\ArsenikGit\\Data")
-
-# Asger wd
-#setwd("/Users/AsgerSturisTang/OneDrive - Danmarks Tekniske Universitet/DTU/4. Semester/Arsenik i GIT/Data")
-
 # Joachim wd
 setwd("/Users/JoachimPorsA/Documents/4. Semester - DTU/Fagprojekt/Data/Arsenik i vand/Data")
-
-#Oskar wd 
-# setwd("C:\\Users\\User\\OneDrive - Danmarks Tekniske Universitet\\SAS_030919\\4. Semester\\42584_Fagprojekt\\Arsenik i drikkevand\\42584_Data\\arsenik_i_vand\\Data")
 
 set.seed(69)
 
@@ -27,12 +18,9 @@ N <- length(blad$events)
 # p.hat (empirisk sandsynlighed)
 p.hat <- sum(blad$events/N)
 
-
 # Her defineres modellen:
-analysis <- glm(events ~ log(1 + conc) + age + I(age^2),family=poisson(link=log),data=blad, offset=log(at.risk))
+analysis<-glm(events~log(1 + conc) + age + I(age^2) + ,family=poisson(link=log),data=blad, offset=log(at.risk)) 
 summary(analysis)
-
-
 
 # Hvor god er modellen 
 drop1(analysis, test="Chisq")
@@ -41,53 +29,10 @@ drop1(analysis, test="Chisq")
 prediction.temp<-as.data.frame(predict(analysis,se.fit=T))
 prediction.data<-data.frame(pred=prediction.temp$fit, upper=prediction.temp$fit+ 1.96*prediction.temp$se.fit, lower=prediction.temp$fit-1.96*prediction.temp$se.fit)
 
-# Transformerer dataen tilbage til original tilstand og sorterer data efter pred
-prediction.data.original <- exp(prediction.data)
-prediction.data.original <- prediction.data.original[order(prediction.data.original$pred),]
-
-
-# Sorterede plots i original data transformation 
-plot(prediction.data.original$pred, blad$events[order(prediction.data.original$pred)], col="blue")
-lines(prediction.data.original$lower, blad$events[order(prediction.data.original$pred)], col="red")
-lines(prediction.data.original$upper, blad$events[order(prediction.data.original$pred)], col="red")
-
-
-# Laver foreløbig test, tror det her er den rigtige måde at plotte det på
-cbind(blad$events[order(blad$events, decreasing = TRUE)],round(prediction.data.original$pred,1))
-plot(blad$events[order(blad$events, decreasing = TRUE)],round(prediction.data.original$pred,0))
-
-
-#plot i log transformation 
-plot(prediction.data$pred, blad$events[order(prediction.data$pred)], col="blue")
-lines(prediction.data$lower, blad$events, col="red")
-lines(prediction.data$upper, blad$events, col="red")
-
-# Histogram over log transformeret procent for at få kræft
-hist(log(blad$events/blad$at.risk))
-
-# Data til histogrammet oven over
-cbind(blad[blad$events>4,] ,log(blad$events/blad$at.risk)[blad$events > 4])
-
-# Taylor udvider til ny analysis
-analysis2 <- update(analysis, ~.+I(1/2*conc^2)+I(1/2*age^2))
-analysis2 <- update(analysis2, ~.+I(1/3*conc^3))
-summary(analysis2)
-
-# Hvor god er modellen 
-drop1(analysis2, test="Chisq")
-
-# Laver signifikans niveauer 
-prediction.temp<-as.data.frame(predict(analysis2,se.fit=T))
-prediction.data<-data.frame(pred=prediction.temp$fit, upper=prediction.temp$fit+ 1.96*prediction.temp$se.fit, lower=prediction.temp$fit-1.96*prediction.temp$se.fit)
-
-prediction.data.original <- exp(prediction.data)
-prediction.data.original <- prediction.data.original[order(blad$events, decreasing = TRUE),]
-
-
-# plots i original data transformation 
-#plot(prediction.data.original$pred, blad$events, col="blue")
-#lines(prediction.data.original$lower, blad$events, col="red")
-#lines(prediction.data.original$upper, blad$events, col="red")
+# Residual sum of squares (SSE)
+xxi <- prediction.data[1]
+xxgen <- mean(blad$events)
+sum((xxi - xxgen)^2)
 
 maxr <- 5
 
@@ -103,16 +48,8 @@ blad$events<- blad$events[order(blad$events, decreasing = TRUE)]
 for (i in 1:length(prediction.data.original$pred)){
   res <- mean(blad$events[0.1*(i-1) <= prediction.data.original$pred & prediction.data.original$pred < 0.1*i])
   v = c(v,res)
-  x = c(x, 0.1*i)
-}
+  x = c(x, 0.1*i)}
 
 plot(x, v, xlim=c(0, maxr), ylim=c(0, maxr))
 lines(0:maxr,0:maxr, type="l")
-
-
-
-#plot i log data 
-#plot(prediction.data$pred, blad$events[order(prediction.data$pred)], col="blue")
-#lines(prediction.data$lower, blad$events, col="red")
-#lines(prediction.data$upper, blad$events, col="red")
 
