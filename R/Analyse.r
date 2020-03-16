a01d8636d393acac1c258fa90ae8c77bbb6df8d0
TheCocoMaan <joachimpors@hotmail.com>
2020-03-16 12:53:56 +0100

    uploaded dokument

--- R/Analyse.r
+++ R/Analyse.r
@@ -1,51 +1,77 @@
-library(lattice)
-
-
-
+library(mgcv)
 # Frederik wd
-# setwd("C:\\Users\\frede\\OneDrive\\Dokumenter\\DTU\\4. Semester\\Fagprojekt\\Data")
-
+#setwd("C:\\Users\\frede\\OneDrive\\Dokumenter\\DTU\\4. Semester\\Fagprojekt\\ArsenikGit\\Data")
 # Asger wd
-# setwd("/Users/AsgerSturisTang/OneDrive - Danmarks Tekniske Universitet/DTU/4. Semester/Arsenik i vand/Data")
+#setwd("/Users/AsgerSturisTang/OneDrive - Danmarks Tekniske Universitet/DTU/4. Semester/Arsenik i GIT/Data")
+# Joachim wd
+setwd("/Users/JoachimPorsA/Documents/4. Semester - DTU/Fagprojekt/Data/Arsenik i vand/Data")
+#Oskar wd 
+# setwd("C:\\Users\\User\\OneDrive - Danmarks Tekniske Universitet\\SAS_030919\\4. Semester\\42584_Fagprojekt\\Arsenik i drikkevand\\42584_Data\\arsenik_i_vand\\Data")
+set.seed(69)
 
+############## INDLÆSNING AF DATA ################
 fblad <- read.table("fblad.sw.dat", header=TRUE)
-head(fblad)
-fblad1 <- fblad[c(15:549), ]
+mblad <- read.table("mblad.sw.dat", header=TRUE)
+fblad$gender <- "Female"
+mblad$gender <- "Male"
+# fblad$female <- 1
+# mblad$female <- 0
+blad <- rbind(fblad,mblad)
 
-N <- length(fblad1$events)
+# Antal observationer
+N <- length(blad$events)
 
-p.hat <- sum(fblad1$events/N)
+# p.hat (empirisk sandsynlighed)
+p.hat <- sum(blad$events/N)
 
-# sæt en formel til glm
-var <- fblad1$conc + fblad$age
+############### General Additive Model // Model-definering ##############
+analysis <- glm(events~log(1+conc) + age + I(age^2), family=poisson(link= "log"), data=blad)
+# analysis <- gam(events~gender+s(age)+s(age,by=female)+s(conc)+
+#       s(age,conc)+offset(I(log(at.risk))),
+#     family=poisson(link = "log"),
+#     data=blad)
+summary(analysis)
 
-analysis<-glm(events~var,family=poisson(link=log),data=fblad1) 
-
-# Hvor god er modellen 
-drop1(analysis, test="Chisq")
 
 # Laver signifikans niveauer 
 prediction.temp<-as.data.frame(predict(analysis,se.fit=T))
 prediction.data<-data.frame(pred=prediction.temp$fit, upper=prediction.temp$fit+ 1.96*prediction.temp$se.fit, lower=prediction.temp$fit-1.96*prediction.temp$se.fit)
 
+# Transformerer dataen tilbage til original tilstand og sorterer data efter pred
+prediction.data.original <- exp(prediction.data)
+prediction.data.original <- prediction.data.original[order(prediction.data.original$pred),]
 
-prediction.data.original<-exp(prediction.data)
+# Laver signifikans niveauer 
+prediction.temp<-as.data.frame(predict(analysis2,se.fit=T))
+prediction.data<-data.frame(pred=prediction.temp$fit, upper=prediction.temp$fit+ 1.96*prediction.temp$se.fit, lower=prediction.temp$fit-1.96*prediction.temp$se.fit)
 
-# plots i log transformation 
-plot(var,prediction.data$pred, col="blue")
-lines(var,prediction.data$lower, col="red")
-lines(var,prediction.data$upper, col="red")
-
-
-#index for undersøgelses punkt 
-i1 <- 255
-# sandsynlighhed for at få kræft i indexpunktet 
-p1 <- fblad1$events[i1]/fblad1$at.risk[i1]*100
-#plot af sandsynlighed for kræft i orginal data 
-plot(prediction.data.original$pred,var)
-lines(prediction.data.original$lower,var, col="red")
-lines(prediction.data.original$upper,var, col="red")
-points(var[i1],p1,col="green")
+prediction.data.original <- exp(prediction.data)
+prediction.data.original <- prediction.data.original[order(blad$events, decreasing = TRUE),]
 
 
+############# PLOT #################
 
+maxr <- 100
+
+# Laver foreløbig test, tror det her er den rigtige måde at plotte det på
+plot(round(prediction.data.original$pred,2),blad$events[order(blad$events, decreasing = TRUE)], xlim=c(0, maxr), ylim=c(0, maxr))
+lines(0:maxr,0:maxr, type="l")
+
+v = vector()
+x = vector()
+
+blad$events<- blad$events[order(blad$events, decreasing = TRUE)]
+
+for (i in 1:length(prediction.data.original$pred)){
+  res <- mean(blad$events[0.1*(i-1) <= prediction.data.original$pred & prediction.data.original$pred < 0.1*i])
+  v = c(v,res)
+  x = c(x, 0.1*i)
+}
+
+sd.Pred <- sd(prediction.data.original$pred)
+
+length(prediction.data.original$upper)
+plot(x, v, xlim=c(0, maxr))
+lines(0:maxr,0:maxr, type="l")
+lines(0:maxr+sd.Pred,0:maxr, type="l", col = "red")
+lines(0:maxr-sd.Pred,0:maxr, type="l", col = "red")
