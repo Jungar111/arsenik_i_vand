8ba923e8cbf7b330e1671c5bc4db5b4f1fe78742
TheCocoMaan <joachimpors@hotmail.com>
2020-03-30 14:13:28 +0200

    SEJT PLOT!

--- R/Analyse.r
+++ R/Analyse.r
@@ -1,4 +1,5 @@
 library(mgcv)
+library(ggplot2)
 # Frederik wd
 #setwd("C:\\Users\\frede\\OneDrive\\Dokumenter\\DTU\\4. Semester\\Fagprojekt\\ArsenikGit\\Data")
 # Asger wd
@@ -38,6 +39,9 @@ par(mfrow=c(2,1))
 plot(analysis$residuals, col=lun$group, main = "Indbygget R funktion")
 plot(analysis$fitted.values, (lun$events - analysis$fitted.values) / sqrt(analysis$fitted.values), col=lun$group, main = "Manuel beregning")
 par(mfrow=c(1,1))
+## Kan det forsvares dette plot? Og hvor mange er ude for vores konfidensinterval ud af 1118?
+length(((lun$events - analysis$fitted.values) / sqrt(analysis$fitted.values))[(lun$events - analysis$fitted.values)/sqrt(analysis$fitted.values) > 4])
+1 - pnorm(3.91)^(1118 - 12)
 
 # Laver signifikans niveauer 
 prediction.temp<-as.data.frame(predict(analysis, se.fit=T))
@@ -62,13 +66,87 @@ lines(0:maxr-sd.Pred, 0:maxr, type="l", col = "red")
 v = vector()
 x = vector()
 
-lun$events <- lun$events[order(lun$events, decreasing = TRUE)]
+# lun$events <- lun$events[order(lun$events, decreasing = TRUE)]
 
 for (i in 1:length(prediction.data.original$pred)){
-  res <- mean(lun$events[0.05*(i-1) <= prediction.data.original$pred & prediction.data.original$pred < 0.05*i])
+  res <- mean(lun$events[0.02*(i-1) <= prediction.data.original$pred & prediction.data.original$pred < 0.02*i])
   v = c(v, res)
-  x = c(x, 0.05*i)}
+  x = c(x, 0.02*i)}
 
 plot(x, v, xlim=c(0, maxr), ylim=c(0, maxr), xlab="Average predicted events", ylab="Average actual events")
 lines(0:maxr,0:maxr, type="l")
+
+
+############# risk vs age plottet - den hellige gral. ###############
+to <- 82.5
+lun.pred <- data.frame(conc = rep(0, to+1),
+                       age = 0:to,
+                       at.risk=100,
+                       gender = rep("Male", to+1),
+                       female = rep(0, to+1),
+                       village1 = rep(0, to+1))
+predict <- predict(analysis, newdata = lun.pred, se.fit=TRUE)
+
+lun.pred2 <- data.frame(conc = rep(0, to+1),
+                       age = 0:to,
+                       at.risk=100,
+                       gender = rep("Female", to+1),
+                       female = rep(1, to+1),
+                       village1 = rep(0, to+1))
+predict2 <- predict(analysis, newdata = lun.pred2, se.fit=TRUE)
+
+##### Concentration = 448
+lun.pred3 <- data.frame(conc = rep(448, to+1),
+                       age = 0:to,
+                       at.risk=100,
+                       gender = rep("Male", to+1),
+                       female = rep(0, to+1),
+                       village1 = rep(1, to+1))
+predict3 <- predict(analysis, newdata = lun.pred3, se.fit=TRUE)
+
+lun.pred4 <- data.frame(conc = rep(448, to+1),
+                        age = 0:to,
+                        at.risk=100,
+                        gender = rep("Female", to+1),
+                        female = rep(1, to+1),
+                        village1 = rep(1, to+1))
+predict4 <- predict(analysis, newdata = lun.pred4, se.fit=TRUE)
+
+####### Concentration = 934
+lun.pred5 <- data.frame(conc = rep(934, to+1),
+                       age = 0:to,
+                       at.risk=100,
+                       gender = rep("Male", to+1),
+                       female = rep(0, to+1),
+                       village1 = rep(1, to+1))
+predict5 <- predict(analysis, newdata = lun.pred5, se.fit=TRUE)
+
+lun.pred6 <- data.frame(conc = rep(934, to+1),
+                        age = 0:to,
+                        at.risk=100,
+                        gender = rep("Female", to+1),
+                        female = rep(1, to+1),
+                        village1 = rep(1, to+1))
+predict6 <- predict(analysis, newdata = lun.pred6, se.fit=TRUE)
+
+plot(exp(predict$fit), xlim=c(0,82.5), ylim=c(0,0.60), type = "l", col="blue", xlab = "Age in years", ylab = "Risk of dying from lung cancer (in %)", main="Events at different conc. and ages")
+lines(exp(predict2$fit), lty=1, col="red")
+lines(exp(predict3$fit), lty=2, col="blue")
+lines(exp(predict4$fit), lty=2, col="red")
+lines(exp(predict5$fit), lty=3, col="blue")
+lines(exp(predict6$fit), lty=3, col="red")
+legend(5, 0.55, legend=c("Male", "Female", "0 ppm", "448 ppm", "934 ppm"),
+       col=c("blue", "red", 1, 1, 1), lty=c(1,1,1,2,3), cex=0.8)
+
+########### Conc plot og age plot ###############
+lun.pred2 <- data.frame(conc = lun$conc,
+                        age = lun$age,
+                        pop = lun$at.risk,
+                        cases = lun$events,
+                        pred.cases = prediction.data.original$pred)
+
+ggplot(lun.pred2, aes(x = conc)) + geom_point(aes(y = cases/pop*100), size = 1) + geom_point(aes(y = pred.cases/pop*100), colour = "red", size = 1, alpha = 0.5)
+
+ggplot(lun.pred2, aes(x = age)) + geom_point(aes(y = cases/pop*100), size = 1) + geom_point(aes(y = pred.cases/pop*100), colour = "red", size = 1, alpha = 0.5)
+
 
