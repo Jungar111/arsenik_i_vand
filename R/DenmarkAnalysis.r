460e59bcc980b0f2b616e1624886828b7772b97b
TheCocoMaan <joachimpors@hotmail.com>
2020-06-15 10:48:15 +0200

    p

--- R/DenmarkAnalysis.r
+++ R/DenmarkAnalysis.r
@@ -217,6 +217,7 @@ for (j in 1:length(conclis)){
     colIndex <- colIndex + 1
   }
 }
+plot(0:21*43,EL, type="l", main="Excess Risk Profile", xlab="Concentration in ppb", ylab="Excess risk factor", col="darkblue")
 
 
 plot(conclis, testListMale, col = "blue", main="Lifetime probability of dying from lung cancer \n with excess risk profile (DK)", xlab="Concentration in ppb", ylab="Lifetime probability", ylim=c(0,0.25), pch=20)
@@ -227,36 +228,3 @@ lines(conclis, rep(m$Rlung0, length(conclis)), col = "blue")
 lines(conclis, rep(f$Rlung0, length(conclis)), col = "red")
 legend("topleft", legend = c("Male", "Female", "Male Baseline", "Female Baseline"), col = c("blue", "red", "blue", "red"), pch=c(20,20,NA,NA), lty = c(0,0,1,1), cex=0.7)
 #
-
-##### NOTER #####
-### 2A-23:
-# Hvis man ved at person har overlevet til t0 (i dette tilfælde er t0 = 1) år, hvad er så sandsynligheden for at dø af lungekræft.
-sum((m$hlist[i]/m$hslist[i]) * m$kombi[i]) * 100
-
-## qlist er chancen for at overleve til næste aldersgruppe (komme videre fra sin egen)!
-## qi is the prob of surviving year i when all causes are acting.
-
-# village1 = 0 skulle gerne have lidt overdødelighed og
-# village1 = 1 skulle gerne ligne DANMARK meget.
-
-# Hazard ratio = odds ratio (ikke matematisk ens men man behandler dem ens) og risk ratio og odds ratio er næsten identisk når sandsynlighederne er så små!
-# God argumentation!
-
-lun.predxx <- data.frame(conc = rep(10, to+1),
-                         age = 0:to,
-                         at.risk=100,
-                         gender = rep("Male", to+1),
-                         female = rep(0, to+1),
-                         village1 = rep(0, to+1))
-predictxx <- predict(analysis, newdata = lun.predxx, se.fit=TRUE)
-
-
-Rlunge <- 0
-Elist <- predictxx$se.fit[seq(1, length(predict1$se.fit), 4)]
-for (i in 1:19){
-  for (k in i-1){
-    Rlunge <- Rlunge + (hlist[i]*(1+Elist[i]) / hslist[i]+hlist[i]*Elist[i])* Slist[i] * (1-qlist[i] * exp(-hlist[i]*Elist[i])) * exp(-sum(hlist[k]*Elist[k]))
-  }
-}
-Rlunge
-
